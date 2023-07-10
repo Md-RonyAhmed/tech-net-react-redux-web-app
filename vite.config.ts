5206e83470f3cd7671f59446eaa7d0b2cd48884a
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-07-11 01:20:54 +0600

    [DONE] terget deleted

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  build: {
+    target: 'es2019',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
