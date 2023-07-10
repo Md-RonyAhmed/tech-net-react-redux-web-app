9b3ef95f19d90ac04523f1446e39bab76b5a3ce0
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-07-10 23:50:58 +0600

    [DONE] checkout page

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -14,7 +14,12 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 import { useAppDispatch, useAppSelector } from '@/redux/hook';
-import { addToCart, removeFromCart, removeOne } from '@/redux/features/cart/cartSlice';
+import {
+  addToCart,
+  removeFromCart,
+  removeOne,
+} from '@/redux/features/cart/cartSlice';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
   const { products, total } = useAppSelector((state) => state.cart);
@@ -23,6 +28,7 @@ export default function Cart() {
     (total, product) => total + product.quantity!,
     0
   );
+
   return (
     <Sheet>
       <SheetTrigger>
@@ -34,22 +40,27 @@ export default function Cart() {
       <SheetContent className="overflow-auto relative">
         <SheetHeader>
           <SheetTitle>Cart</SheetTitle>
-          <h1>Total: {total.toFixed(2)} Bdt</h1>
+          <h1 className="text-lg pb-4">
+            Total: <span className="font-bold">{total.toFixed(2)}</span> Bdt
+          </h1>
         </SheetHeader>
-        <div className="space-y-5">
+        <div className="space-y-6">
           {products.map((product) => (
             <div
-              className="border h-44 p-5 flex justify-between rounded-md"
-              key={product.name}
+              className="border h-60 p-5 pl-0 flex justify-between rounded-md"
+              key={product._id}
             >
               <div className="border-r pr-5 shrink-0">
                 <img src={product?.image} alt="" className="h-full" />
               </div>
               <div className="px-2 w-full flex flex-col gap-3">
-                <h1 className="text-2xl self-center">{product?.name}</h1>
+                <h1 className="text-lg self-center">{product?.model}</h1>
                 <p>Quantity: {product.quantity}</p>
-                <p className="text-xl">
-                  Total Price: {(product.price * product.quantity!).toFixed(2)}{' '}
+                <p className="text-base">
+                  Total Price:{' '}
+                  <span className="font-bold">
+                    {(product.price * product.quantity!).toFixed(2)}
+                  </span>{' '}
                   Bdt
                 </p>
               </div>
@@ -60,7 +71,8 @@ export default function Cart() {
                 <Button onClick={() => dispatch(removeOne(product))}>
                   <HiMinus size="20" />
                 </Button>
-                <Button onClick={() => dispatch(removeFromCart(product))}
+                <Button
+                  onClick={() => dispatch(removeFromCart(product))}
                   variant="destructive"
                   className="bg-red-500 hover:bg-red-400"
                 >
@@ -69,6 +81,11 @@ export default function Cart() {
               </div>
             </div>
           ))}
+        </div>
+        <div className="pt-4">
+          <Button variant="default" asChild>
+            <Link to="/checkout">Checkout</Link>
+          </Button>
         </div>
       </SheetContent>
     </Sheet>
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Button } from '@/components/ui/button';
 import { DatePickerWithPresets } from '@/components/ui/datePickerWithPreset';
 import { Input } from '@/components/ui/input';
@@ -14,36 +16,55 @@ export default function Checkout() {
 
   const { products } = useAppSelector((state) => state.cart);
 
+  // Calculate subtotal
+  const subTotal = products.reduce(
+    (acc, product) => acc + product.price * product.quantity!,
+    0
+  );
+
+  // Calculate delivery fee (1% of subtotal)
+  const deliveryFee = Number((subTotal * 0.01).toFixed(2));
+
+  // Calculate total
+  const total = (subTotal + deliveryFee).toFixed(2);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-80px)] gap-10 text-primary">
+    <form
+      className="flex justify-center items-center h-[calc(100vh-80px)] gap-10 text-primary"
+      onSubmit={handleSubmit}
+    >
       <div className="max-w-3xl w-full">
-        <h1 className="mb-2">Delivery Information</h1>
+        <h1 className="mb-2 font-bold">Delivery Information</h1>
         <div className="h-[60vh] border border-gray-300 rounded-md p-10 overflow-auto">
           <div className="flex gap-5">
             <div className="w-full space-y-5">
               <div>
-                <Label htmlFor="name">Name</Label>
-                <Input type="text" id="name" className="mt-2" />
+                <Label htmlFor="name">Name<sup className='text-red-500 text-base'>*</sup></Label>
+                <Input type="text" id="name" className="mt-2" required />
               </div>
               <div>
-                <Label htmlFor="name">Email</Label>
-                <Input type="text" id="name" className="mt-2" />
+                <Label htmlFor="email">Email<sup className='text-red-500 text-base'>*</sup></Label>
+                <Input type="email" id="email" className="mt-2" required/>
               </div>
             </div>
             <div className="w-full space-y-5">
               <div>
-                <Label htmlFor="name">Phone</Label>
-                <Input type="text" id="name" className="mt-2" />
+                <Label htmlFor="phone">Phone<sup className='text-red-500 text-base'>*</sup></Label>
+                <Input type="number" id="phone" className="mt-2" required/>
               </div>
               <div>
-                <Label htmlFor="name">City</Label>
-                <Input type="text" id="name" className="mt-2" />
+                <Label htmlFor="name">City<sup className='text-red-500 text-base'>*</sup></Label>
+                <Input type="text" id="name" className="mt-2" required/>
               </div>
             </div>
           </div>
           <div className="mt-5">
-            <Label htmlFor="name">Address</Label>
-            <Textarea id="name" className="mt-2" />
+            <Label htmlFor="address">Address<sup className='text-red-500 text-base'>*</sup></Label>
+            <Textarea id="address" className="mt-2" required/>
           </div>
           <div className="flex items-center gap-2 mt-5">
             <Label className="text-lg">Scheduled Delivery</Label>
@@ -92,7 +113,7 @@ export default function Checkout() {
         </div>
       </div>
       <div className="max-w-lg w-full">
-        <h1 className="mb-2">Order Summery</h1>
+        <h1 className="mb-2 font-bold">Order Summery</h1>
         <div className="border border-gray-300 rounded-md h-[60vh] p-10 flex flex-col">
           <div className="flex-grow  mb-2 space-y-2 overflow-auto">
             {products.map((product) => (
@@ -117,20 +138,20 @@ export default function Checkout() {
           <div className="space-y-2">
             <div className="flex justify-between text-lg">
               <p>Subtotal</p>
-              <p>77.90$</p>
+              <p>{subTotal} Bdt</p>
             </div>
             <div className="flex justify-between text-lg">
               <p>Delivery</p>
-              <p>4.5$</p>
+              <p>{deliveryFee} Bdt</p>
             </div>
             <div className="flex justify-between text-xl font-bold">
               <p>Total</p>
-              <p>81.95$</p>
+              <p>{total} Bdt</p>
             </div>
-            <Button className="w-full">Checkout</Button>
           </div>
+          <Button className="w-full my-3">Checkout</Button>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
