ceb2404b365e61f45c1985886e795be901a28d4d
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-07-10 16:46:17 +0600

    [DONE] product details page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,23 +1,32 @@
 import ProductReview from '@/components/ProductReview';
+import Error from '@/components/ui/Error';
+import Loading from '@/components/ui/Loading';
 import { Button } from '@/components/ui/button';
-import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
+import { useGetProductQuery } from '@/redux/api/apiSlice';
+import { addToCart } from '@/redux/features/cart/cartSlice';
+import { useAppDispatch } from '@/redux/hook';
 import { useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
   const { id } = useParams();
+  const { data: product, isLoading, isError } = useGetProductQuery(id);
+  const dispatch = useAppDispatch();
 
-  //! Temporary code, should be replaced with redux
-  const [data, setData] = useState<IProduct[]>([]);
-  useEffect(() => {
-    fetch('../../public/data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  if (isLoading && !product) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
 
-  const product = data?.find((item) => item._id === Number(id));
-
-  //! Temporary code ends here
+  if (!isLoading && isError) {
+    return (
+      <div>
+        <Error error="Error fetching product" />
+      </div>
+    );
+  }
 
   return (
     <>
@@ -26,14 +35,36 @@ export default function ProductDetails() {
           <img src={product?.image} alt="" />
         </div>
         <div className="w-[50%] space-y-3">
-          <h1 className="text-3xl font-semibold">{product?.name}</h1>
+          <h1 className="text-3xl font-semibold">{product?.model}</h1>
           <p className="text-xl">Rating: {product?.rating}</p>
-          <ul className="space-y-1 text-lg">
-            {product?.features?.map((feature) => (
+          <p className="text-xl">
+            Availability:{' '}
+            {product?.status ? (
+              <span className="text-blue-600 bg-blue-100 p-1 rounded-md">
+                In stock
+              </span>
+            ) : (
+              <span className="text-red-600 bg-red-200 p-1 rounded-md">
+                Out of stock
+              </span>
+            )}
+          </p>
+          <p className="text-xl">
+            Price: <span className="font-bold">{product?.price}</span> Bdt
+          </p>
+          <h1 className='font-bold text-xl'>Key Features:</h1>
+          <ul className="space-y-1 text-lg list-disc">
+            {product?.keyFeature?.map((feature: string) => (
               <li key={feature}>{feature}</li>
             ))}
           </ul>
-          <Button>Add to cart</Button>
+          <Button
+            variant="default"
+            disabled={!product?.status}
+            onClick={() => dispatch(addToCart(product))}
+          >
+            Add to cart
+          </Button>
         </div>
       </div>
       <ProductReview />
